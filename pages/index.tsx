ea1234ec361c1d70b71e0ea4b2b1e17a13da83d0
MagicScripta <63019472+MagicScripta@users.noreply.github.com>
2022-03-01 06:59:58 -0330

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,15 +75,13 @@ const experiencesPage = (
           color: aquamarine;
         `}
       >
-        I automated the process of posting data to a confidential website
+        Worked on a website with JavaScript at its core.
       </h3>
       <p>
-        The content to be posted was uploaded to google spreadsheets by hand
-        with varying intervals, someone else would then copy the data from the
-        excel spreadsheet and fill it into a form on a website. I automated
-        checking the google spreadsheet for changes and posting the data to the
-        website. I increased efficiency and eliminated a step in the flow of
-        data that could cause unintended changes.
+        •	Used the ReactJS, NodeJS and NextJS. 
+        •	Played a major role in its design with HTML and CSS, 
+        •	Coordinated its interactivity with TypeScript
+        •	Populated the data on it with Python web scraping.
       </p>
     </span>
     <span>
@@ -92,14 +90,12 @@ const experiencesPage = (
           color: aquamarine;
         `}
       >
-        I reduced time taken to create custom directories for different
-        files(November 2020)
+        Automated the process of posting data to a website
       </h3>
       <p>
-        I wrote a python script to read files, depending on parameters
-        specifying the file types and names that required working directories.
-        After creating A working directory for a file, all other associated
-        files are brought to the working directory.
+        •	Automated the management of data on the website, thereby increasing efficiency and eliminating redundancy that could cause unintended changes.
+        •	Reduced time taken to create custom directories for different files
+
       </p>
     </span>
     <span>
@@ -108,14 +104,22 @@ const experiencesPage = (
           color: aquamarine;
         `}
       >
-        Increased trade opening and closing speed by automating signals from
-        telegram into Binance (July 2020)
+        Increased trade opening and closing speed by automating signals from telegram into Binance 
       </h3>
       <p>
-        Signals received by the telethon API are processed, to extract the
-        opening price and closing prices of an order. The scripts execute the
-        opening at the specified price with a percentage of the balance that has
-        been pre-set.
+        •	Created a telethon API to send signals which are processed to extract the opening and closing prices of orders.
+        •	Scripts created were able to execute the opening at the specified price with a percentage of the balance that has been pre-set.
+      </p>
+      <h3
+        css={css`
+          color: aquamarine;
+        `}
+      >
+        Automated call dialing and receipt using the Twilio API with Python 
+      </h3>
+      <p>
+        •	Reduced human error by automating the explanation of instructions.
+        •	Reduced time taken to make verification through phone calls by automating the process.
       </p>
     </span>
   </>
@@ -123,10 +127,15 @@ const experiencesPage = (
 
 const skillsPage = (
   <span>
-    <h3>➼ Web Scraping with Python <br/>&emsp;&emsp;— Over one year experience</h3>
-    <h3>➼ Web Automation with Python <br/>&emsp;&emsp;— Over one year experience</h3>
-    <h3>➼ Web design with HTML/CSS, Javascript <br/>&emsp;&emsp;— Beginner</h3>
-    <h3>➼ Typing speed of 42 wpm</h3>
+    	Communication< br/>	Teamwork< br/>	Patience< br/>	Accountability< br/>	Attention to detail	Retentive memory< br/>	Positivity< br/>	Problem-Solving< br/>	Curiosity  
+  </span>
+);
+
+const technicalSkillsPage = (
+  <span>
+    <h3>➼ Scripting and Automation <br/>&emsp;&emsp;	Web scraping using Python<br/>&emsp;&emsp;	Web Automation using Python</h3>
+    <h3>➼ Web Development <br/>&emsp;&emsp;	HTML/CSS<br/>&emsp;&emsp;	JavaScript<br/>&emsp;&emsp;	ReactJS<br/>&emsp;&emsp;	NextJS</h3>
+    <h3>➼ Workflow tools <br/>&emsp;&emsp;	Git</h3>
   </span>
 );
 
@@ -145,7 +154,7 @@ const educationPage = (
       </p>
       Bsc. Computer Science
       <br />
-      Sept 2020 – 2024
+       In view (2024)
     </span>
     <br />
     <br />
@@ -167,23 +176,7 @@ const educationPage = (
       </p>
       Bsc. Computer Science
       <br />
-      Sept 2018 – 2019 (Incomplete)
-    </span>
-    <br />
-    <br />
-    <br />
-    <span>
-      <p
-        className={styles.linked}
-        css={css`
-          transform-origin: left;
-        `}
-      >
-        <a href={"https://www.ielts.org/"} target={"_blank"} rel={"noreferrer"}>
-          IELTS (CEFR - C1)
-        </a>
-      </p>
-      English Language proficiency Certification
+      Transfer (2019)
     </span>
   </>
 );
@@ -233,12 +226,15 @@ const Home: NextPage = () => {
               padding: 2% 4%;
             `}
           >
-            A second-year computer science student with one-year freelance
-            experience looking for technology inclined work experience. I am
-            open to work both remotely and within Canada as an intern between May 2022 and August 2022 both months included. I would love to learn and gain as much experience as I can before my Co-op program in 2023.
+            A second-year computer science student possessing about 2 years of programming and freelance experience, 
+            successfully completing complex projects, using industry standard tools and methodologies.
+            Hands-on experience with python and frontend web design. 
+            A fast learner with strong attention to details, excellent analytical, communication, technical, and interpersonal skills 
+            with the ability to multi-task, learn modern technology quickly, manage and collaborate with others to drive product development. 
+            Looking for junior opportunities for the summer where I can hone my existing skills and contribute to organizational growth.
             <br/>
             <br/>
-            Here are two of my professional profiles you can look at before moving
+            Here are three of my professional profiles you can look at before moving
             on to my relevant experiences.
           </p>
           <div
@@ -253,6 +249,10 @@ const Home: NextPage = () => {
             <ReferenceIcon
               referenceHref={"https://www.linkedin.com/in/stephen-mustapha-ng/"}
               pictureHref={"/LinkedIn.png"}
+            />
+            <ReferenceIcon
+              referenceHref={"https://github.com/MagicScripta"}
+              pictureHref={"/Github.png"}
             />
           </div>
         </div>
