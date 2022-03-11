0191e4da8750c2d574a15f2475446a6a40299918
MagicScripta <63019472+MagicScripta@users.noreply.github.com>
2022-03-11 19:17:41 -0330

    Add Google Analytics code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BMPSWY9L1J"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BMPSWY9L1J');
+      </script>
         <title>Stephen M. Resume</title>
         <link rel="icon" href="/head.png" />
         <meta name="viewport" content="width=device-width"/>
